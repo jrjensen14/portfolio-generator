43aad8a7ea6b5dfa30ba4c943c492c96e0aa3985
JRJ <jjsocc171@msn.com>
2020-12-07 11:53:00 -0600

    updated HTML template

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
    return inquirer.prompt([
         {
@@ -134,21 +137,19 @@ promptUser()
     // .then(answers => console.log(answers))
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        // console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
 
 
 // console.log(inquirer);
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 
 
